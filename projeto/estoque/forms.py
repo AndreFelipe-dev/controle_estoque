72140387dfd93032795b711df090ab6fb0a307bd
Andre Felipe <felipeandre.dev08@gmail.com>
2023-10-25 00:00:34 -0300

    alterações de estilização

--- projeto/estoque/forms.py
+++ projeto/estoque/forms.py
@@ -4,11 +4,12 @@ class CadastroForm(forms.Form):
 
     codigo= forms.IntegerField(required=False,widget=forms.HiddenInput())
     nome= forms.CharField(label='Nome :', max_length=50,
-                          widget=forms.TextInput(attrs={'class': 'form-control', }))
+                          widget=forms.TextInput(attrs={'class': 'form-control',  'placeholder':'nome e sobrenome'}))
     preco = forms.FloatField(label='Preço :',
-                             widget=forms.TextInput(attrs={'class': 'form-control' ,'type': 'number'}))
+                             widget=forms.TextInput(attrs={'class': 'form-control' ,'type': 'number','placeholder':'R$00,00'}))
     quantidade = forms.IntegerField(label='Quantidade:',
-                                    widget=forms.TextInput(attrs={'class': 'form-control' ,'type': 'number'}))
+                                    widget=forms.TextInput(attrs={'class': 'form-control' ,
+                                                                  'type': 'number','placeholder':'0000'}))
 
 
     pass
