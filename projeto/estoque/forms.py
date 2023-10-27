45b53f694c94584b9c58c9080b4fb3e6fdcb8533
Andre Felipe <felipeandre.dev08@gmail.com>
2023-10-27 00:01:25 -0300

    projeto concluído

--- projeto/estoque/forms.py
+++ projeto/estoque/forms.py
@@ -4,7 +4,7 @@ class CadastroForm(forms.Form):
 
     codigo= forms.IntegerField(required=False,widget=forms.HiddenInput())
     nome= forms.CharField(label='Nome :', max_length=50,
-                          widget=forms.TextInput(attrs={'class': 'form-control',  'placeholder':'nome e sobrenome'}))
+                          widget=forms.TextInput(attrs={'class': 'form-control',  'placeholder':'produto'}))
     preco = forms.FloatField(label='Preço :',
                              widget=forms.TextInput(attrs={'class': 'form-control' ,'type': 'number','placeholder':'R$00,00'}))
     quantidade = forms.IntegerField(label='Quantidade:',
